f8a806d30b0c52821e517a84c4fd2d40c68ad375
awsh10 <awsh@ukr.net>
2015-01-28 23:36:25 +0200

    Оькывает айты с содержанием статьи

--- ffw/common_pages/views.py
+++ ffw/common_pages/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from common_pages.models import StaticPage
+
+def index(request):
+    
+    #myObj=Staticpage.objects.(slug=
+    return HttpResponse("Hello, world. You're at the common_pages index.")
+
+def article(request,slug):
+    myObj=StaticPage.objects.get(slug='aaaa')
+    
+    return HttpResponse("Article - " + myObj.text)
+
+
+

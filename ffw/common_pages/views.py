2e73e2d01be0d5064f38ae4afab80847da49d201
awsh10 <awsh@ukr.net>
2015-02-01 16:44:35 +0200

    Use get_object_or_404,render, get_list_or_404

--- ffw/common_pages/views.py
+++ ffw/common_pages/views.py
@@ -1,19 +1,15 @@
-from django.shortcuts import get_object_or_404,render
+from django.shortcuts import get_object_or_404,render, get_list_or_404
 
 from django.http import HttpResponse, Http404
 from common_pages.models import StaticPage
 
 def page_list_get(request):
-    page_list = StaticPage.objects.all().order_by('-created')
+    page_list = get_list_or_404(StaticPage.objects.all().order_by('-created'))
     context = {'page_list' : page_list}
     return render(request, 'common_pages/page_list.html', context)
     
-def page_get(request,slug):
-    try:
-        page = StaticPage.objects.get(slug = slug)
-    except StaticPage.DoesNotExist:
-        raise Http404("Page does not exist")
-    
+def page_get(request,slug):    
+    page = get_object_or_404(StaticPage.objects, slug = slug)
     return HttpResponse(page) 
 
 

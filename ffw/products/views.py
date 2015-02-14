f849eeb6a66e097ebb0652fc01c39e4a3afb9e7e
Pavel <marchukpavelp@gmail.com>
2015-02-15 00:43:05 +0200

    Correct gallery small issues

--- ffw/products/views.py
+++ ffw/products/views.py
@@ -9,9 +9,8 @@ class HomeView(View):
 
     def get(self, request):
         categories = models.Category.objects.all().select_related('subcategories')
-        top =  Banner.objects.get(name='top')
-        main = Banner.objects.get(name='main')       
-        images = main.images.all()
+        top = Banner.objects.get(name='top')
+        main = Banner.objects.get(name='main')
         return render(request, 'products/home.html', {'categories': categories,
                       'top': top, 'main': main})
 

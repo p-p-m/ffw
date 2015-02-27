039c82dd05db5f961fc8b958110defa23ef32e62
awsh10 <awsh@ukr.net>
2015-02-27 11:59:21 +0200

    correct error push in products_list.html, url = 127.0.0.1:8000/cart/ - dont work

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,11 +1,10 @@
 
 document.getElementById('product_add').onclick = function() {
     alert(this.value);
-
-
+    
     $.ajax({
       type: "POST",
-      url: "/cart/",
+      url: "127.0.0.1:8000/cart/",
       data: {
           'product_id': 1
       },
@@ -15,7 +14,7 @@ document.getElementById('product_add').onclick = function() {
         alert('Ok');   
       },
       error: function(data) {
-        alert('error');  
+        alert('My error');  
       }      
     });
   

67ac72819492788916c27fd26e6a5e5ffbb79639
awsh10 <awsh@ukr.net>
2015-05-16 00:16:56 +0300

    add method 'add' to var 'cart'

--- ffw/cart/static/cart/cart.js
+++ ffw/cart/static/cart/cart.js
@@ -2,6 +2,56 @@ var cart = {
     'products': {},
     'sum': 0,
     'count': 0,
+    'add': function(product_pk) {
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = $.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+
+        var csrftoken = getCookie('csrftoken');
+
+        function csrfSafeMethod(method) {
+            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+        }
+
+        $.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                }
+            }
+        });
+
+        $.ajaxPrefilter( function( options ) {
+            options.url = $('div#cart').data('url');
+        });
+
+        $.ajax({
+            type: "POST",
+            data:{
+                'product_pk': product_pk
+            },
+            dataType: 'text'
+        })
+        .done(function(data) {
+            var obj = $.parseJSON(data);
+            $('div.cart-count').text(obj.count_cart);
+            $('span.sum').text(obj.sum_cart + ' грн');
+            alert("status - " + obj.status);
+        });
+    },
+
     // Action  can be: "clear" - clear the cart, "remove" - remove the product from the cart,
         // 'add' (or any string) - add the product to the cart.
     cart_change: function(product_pk, action) {
@@ -82,7 +132,7 @@ var cart = {
                 var obj = $.parseJSON(data);
                 $('div.cart-count').text(obj.count_cart);
                 $('span.sum').text(obj.sum_cart + ' грн');
-                alert("status - " + obj.status);                
+                alert("status - " + obj.status);
             });
         };
     }
@@ -90,7 +140,7 @@ var cart = {
 
 
 $(document).ready(function() {
-    
+
     // For cart-display.html
     $('button#remove').on('click', function() {
         var product_pk = this.value;
@@ -100,6 +150,6 @@ $(document).ready(function() {
     //  For products_list.html and product.html
     $('button#buy').on('click', function() {
         var product_pk = this.value;
-        cart.cart_change(product_pk,'add');
+        cart.add(product_pk);
     });
 });

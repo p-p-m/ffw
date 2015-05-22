ef3a628b11594c0a0c55ed44f42396164de9c839
awsh10 <awsh@ukr.net>
2015-05-22 22:13:25 +0300

    class based views

--- ffw/cart/static/cart/cart.js
+++ ffw/cart/static/cart/cart.js
@@ -4,7 +4,7 @@ var cart = {
     'count': 0,
     'url': '',
     'set': function(product_pk, quant) {
-
+        cart.csrf();
 
         $.ajax({
             url: cart.url + 'set/',
@@ -20,7 +20,7 @@ var cart = {
        });
    },
     'remove': function(product_pk) {
-
+        cart.csrf();
 
         $.ajax({
             url: cart.url + 'remove/',
@@ -36,7 +36,7 @@ var cart = {
 
    },
     'clear': function() {
-
+        cart.csrf();
 
         $.ajax({
             url: cart.url,
@@ -56,6 +56,36 @@ var cart = {
        }).done(function(data) {
          alert(data);
        });
+   },
+    'csrf': function() {
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = $.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                   }
+               }
+           }
+            return cookieValue;
+       }
+
+        var csrftoken = getCookie('csrftoken');
+
+        function csrfSafeMethod(method) {
+            return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+       }
+         $.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+               }
+           }
+        });
    },
     'event_success':  function(funct_name='', data) {
         if (funct_name == 'clear') {
@@ -88,8 +118,8 @@ $(document).ready(function() {
     $('button#buy').on('click', function() {
         var product_pk = this.value;
         var quant = $(this).data('quant');
-        //cart.set(product_pk, quant);
+        cart.set(product_pk, quant);
         //cart.get()
-        cart.clear()
+        //cart.clear()
    });
 });

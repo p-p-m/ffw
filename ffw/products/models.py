464e27c08c7535af71c461351e8aff791f9c8e25
awsh10 <awsh@ukr.net>
2015-07-14 21:33:01 +0300

    create field ImageFieldWaterMark

--- ffw/products/models.py
+++ ffw/products/models.py
@@ -2,6 +2,8 @@
 from __future__ import unicode_literals
 
 import logging
+from PIL import Image, ImageDraw, ImageFont, ImageEnhance
+import os, sys
 
 from constance import config
 from django.db import models
@@ -110,6 +112,55 @@ class SubcategoryCharacteristic(models.Model):
     subcategory = models.ForeignKey('Subcategory')
     characteristic = models.ForeignKey(Characteristic)
 
+class ImageFieldWaterMark(models.ImageField):
+    def pre_save(self, model_instance, add):
+        file = super(ImageFieldWaterMark,self).pre_save( model_instance, add)
+        img = Image.open(file.path).convert('RGB')
+        watermark = Image.new('RGBA', img.size, (0,0,0,0))
+        size = 2
+        FONT = 'Arial.ttf'
+        n_font = ImageFont.truetype(FONT, size)
+        text = 'AAAAAAAAAA'
+        n_width, n_height = n_font.getsize(text)
+        while n_width+n_height < watermark.size[0]:
+            size += 2
+            n_font = ImageFont.truetype(FONT, size)
+            n_width, n_height = n_font.getsize(text)
+        draw = ImageDraw.Draw(watermark, 'RGBA')
+        draw.text(((watermark.size[0] - n_width) / 2,
+                      (watermark.size[1] - n_height) / 2),
+                      text, font=n_font)
+        angle = 23
+        watermark = watermark.rotate(angle,Image.BICUBIC)
+        alpha = watermark.split()[3]
+        opacity = 0.25
+        alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
+        watermark.putalpha(alpha)
+        #Image.composite(watermark, img, watermark).save(out_file, 'JPEG')
+        Image.composite(watermark, img, watermark).save(file.path, 'JPEG')
+
+        print type(file), img
+        return file
+
+def add_watermark(in_file, text, out_file='watermark.jpg', angle=23, opacity=0.25):
+    img = Image.open(in_file).convert('RGB')
+    watermark = Image.new('RGBA', img.size, (0,0,0,0))
+    size = 2
+    n_font = ImageFont.truetype(FONT, size)
+    n_width, n_height = n_font.getsize(text)
+    while n_width+n_height < watermark.size[0]:
+        size += 2
+        n_font = ImageFont.truetype(FONT, size)
+        n_width, n_height = n_font.getsize(text)
+    draw = ImageDraw.Draw(watermark, 'RGBA')
+    draw.text(((watermark.size[0] - n_width) / 2,
+              (watermark.size[1] - n_height) / 2),
+              text, font=n_font)
+    watermark = watermark.rotate(angle,Image.BICUBIC)
+    alpha = watermark.split()[3]
+    alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
+    watermark.putalpha(alpha)
+    Image.composite(watermark, img, watermark).save(out_file, 'JPEG')
 
 @python_2_unicode_compatible
 class Subcategory(AbstractCategory):
@@ -121,7 +172,7 @@ class Subcategory(AbstractCategory):
         verbose_name_plural = _('Subcategories')
 
     category = models.ForeignKey(Category, verbose_name=_('Category'), related_name='subcategories')
-    image = models.ImageField(upload_to='subcategories/', verbose_name=_('Image'), blank=True)
+    image = ImageFieldWaterMark(upload_to='subcategories/', verbose_name=_('Image'), blank=True)
     image_small_thumbnail = ImageSpecField(source='image',
                                       processors=[ResizeToFit(width=50, height=50, upscale=True, mat_color='green')],
                                       format='JPEG',

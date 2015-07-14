e1e12b69f9165a0ed0f2bddb4260793f3e8329fd
awsh10 <awsh@ukr.net>
2015-07-15 00:08:56 +0300

    add function for watermarket

--- ffw/products/models.py
+++ ffw/products/models.py
@@ -112,55 +112,40 @@ class SubcategoryCharacteristic(models.Model):
     subcategory = models.ForeignKey('Subcategory')
     characteristic = models.ForeignKey(Characteristic)
 
+
 class ImageFieldWaterMark(models.ImageField):
     def pre_save(self, model_instance, add):
         file = super(ImageFieldWaterMark,self).pre_save( model_instance, add)
-        img = Image.open(file.path).convert('RGB')
+        self.angle = 23
+        self.opacity = 0.25
+        self.mark_text = "TTTTTTTTTT"
+        self.font = 'Arial.ttf'
+        self.__add_watermark(file.path, self.mark_text, self.angle, self.opacity, )
+        return file
+
+    def __add_watermark(self, file_path, mark_text, angle, opacity):
+        img = Image.open(file_path).convert('RGB')
         watermark = Image.new('RGBA', img.size, (0,0,0,0))
         size = 2
-        FONT = 'Arial.ttf'
+        FONT = self.font
         n_font = ImageFont.truetype(FONT, size)
-        text = 'AAAAAAAAAA'
-        n_width, n_height = n_font.getsize(text)
+        n_width, n_height = n_font.getsize(mark_text)
+
         while n_width+n_height < watermark.size[0]:
             size += 2
             n_font = ImageFont.truetype(FONT, size)
-            n_width, n_height = n_font.getsize(text)
+            n_width, n_height = n_font.getsize(mark_text)
+
         draw = ImageDraw.Draw(watermark, 'RGBA')
         draw.text(((watermark.size[0] - n_width) / 2,
                       (watermark.size[1] - n_height) / 2),
-                      text, font=n_font)
-        angle = 23
+                      mark_text, font=n_font)
         watermark = watermark.rotate(angle,Image.BICUBIC)
         alpha = watermark.split()[3]
-        opacity = 0.25
         alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
         watermark.putalpha(alpha)
-        #Image.composite(watermark, img, watermark).save(out_file, 'JPEG')
-        Image.composite(watermark, img, watermark).save(file.path, 'JPEG')
+        Image.composite(watermark, img, watermark).save(file_path, 'JPEG')
 
-        print type(file), img
-        return file
-
-def add_watermark(in_file, text, out_file='watermark.jpg', angle=23, opacity=0.25):
-    img = Image.open(in_file).convert('RGB')
-    watermark = Image.new('RGBA', img.size, (0,0,0,0))
-    size = 2
-    n_font = ImageFont.truetype(FONT, size)
-    n_width, n_height = n_font.getsize(text)
-    while n_width+n_height < watermark.size[0]:
-        size += 2
-        n_font = ImageFont.truetype(FONT, size)
-        n_width, n_height = n_font.getsize(text)
-    draw = ImageDraw.Draw(watermark, 'RGBA')
-    draw.text(((watermark.size[0] - n_width) / 2,
-              (watermark.size[1] - n_height) / 2),
-              text, font=n_font)
-    watermark = watermark.rotate(angle,Image.BICUBIC)
-    alpha = watermark.split()[3]
-    alpha = ImageEnhance.Brightness(alpha).enhance(opacity)
-    watermark.putalpha(alpha)
-    Image.composite(watermark, img, watermark).save(out_file, 'JPEG')
 
 @python_2_unicode_compatible
 class Subcategory(AbstractCategory):

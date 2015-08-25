4742f33de55c1acf09ded75f94d40f8576087b6e
awsh10 <awsh@ukr.net>
2015-08-25 19:43:14 +0300

    correct

--- ffw/cart/views.py
+++ ffw/cart/views.py
@@ -58,7 +58,7 @@ class CartSetView(ResponseView):
 
             cart = Cart(request)
             self._call_cart(cart, product_pk, quant)
-            return HttpResponse(cart.as_json())
+            return self.format_response(cart)
 
 
 class CartAddView(CartSetView):

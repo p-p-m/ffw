f0480e2e65986aa39beab9e5fa7b424b60f414bd
Michael Yakimenko <macgera@gmail.com>
2015-09-11 19:52:13 +0300

    Add equl height for products box in list.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,6 +9,39 @@
         }
     }
 
+    // equal height
+    function equalheight(container) {
+
+        var currentTallest = 0,
+        currentRowStart = 0,
+        rowDivs = new Array(),
+        $el,
+        topPosition = 0;
+
+        $(container).each(function() {
+
+            $el = $(this);
+            $($el).height('auto')
+            topPostion = $el.position().top;
+
+            if (currentRowStart != topPostion) {
+                for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                    rowDivs[currentDiv].height(currentTallest);
+                }
+                rowDivs.length = 0; // empty the array
+                currentRowStart = topPostion;
+                currentTallest = $el.height();
+                rowDivs.push($el);
+            } else {
+                rowDivs.push($el);
+                currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+            }
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                    rowDivs[currentDiv].height(currentTallest);
+            }
+        });
+    }
+
     // Tabs
     function Tabs() {
         var tabContainer = $('[data-role="tab"]');
@@ -646,6 +679,7 @@
         sideScroll();
         priceInput();
         regularPopup();
+        equalheight('[data-view="grid"] .products-list .product');
     });
 
     // all initial on window resize
